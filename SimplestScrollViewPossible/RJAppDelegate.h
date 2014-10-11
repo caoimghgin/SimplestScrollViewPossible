f87ee43f6f1c266978cabf8f1203e39503fb8d76
Kevin Muldoon <caoimghgin@gmail.com>
2014-10-11 16:18:34 -0400

    Initial Commit

---
+++ SimplestScrollViewPossible/RJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RJAppDelegate.h
+//  SimplestScrollViewPossible
+//
+//  Created by Kevin Muldoon on 10/11/14.
+//  Copyright (c) 2014 Robot Jackalope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimplestScrollViewPossible/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimplestScrollViewPossible
+//
+//  Created by Kevin Muldoon on 10/11/14.
+//  Copyright (c) 2014 Robot Jackalope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RJAppDelegate class]));
+    }
+}
